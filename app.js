8265d78090d43251f3726b249ef56e64f5b1c2b9
Kartik Narang <narangk007@gmail.com>
2024-01-07 12:13:54 +0530

    Sign Up Backend

--- app.js
+++ app.js
@@ -1,31 +1,30 @@
 const express = require("express");
 const app = express();
-
 const bodyParser = require("body-parser");
 const path = require("path");
 const fs = require("fs");
-
 const cors = require("cors");
-app.use(cors());
-
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 const dotenv = require("dotenv");
 dotenv.config();
-
 const sequelize = require("./util/database");
-
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 //Router
 const userRouter = require("./router/userRouter");
 //Models
 
 //Middleware
 app.use("/", userRouter);
+app.use("/user", userRouter);
 
 sequelize
-  .sync()
+  .sync()  
   .then((result) => {
     app.listen(process.env.PORT || 3000);
   })
--- router/userRouter.js
+++ router/userRouter.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-
 const userController = require("../controller/userController");
 const userAuthentication = require("../middleware/auth");
 
 router.get("/", userController.getLoginPage);
+
+router.post("/signUp", userController.postUserSignUp);
 
 module.exports = router;
