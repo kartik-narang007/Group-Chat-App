e6b7e3043bebc03aca068ac2148ff2eb64dece3a
Kartik Narang <narangk007@gmail.com>
2024-01-07 12:26:58 +0530

    Login Frontend And Backend

--- app.js
+++ app.js
@@ -26,6 +26,6 @@ app.use("/user", userRouter);
 sequelize
   .sync()  
   .then((result) => {
-    app.listen(process.env.PORT || 3000);
+    app.listen(process.env.PORT || 4000);
   })
   .catch((err) => console.log(err));
