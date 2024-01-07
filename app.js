dddce6ba29a9e2b7450b7dc9a5b527bd6fd22285
Kartik Narang <narangk007@gmail.com>
2024-01-07 12:31:51 +0530

    HomePage Frontend

--- app.js
+++ app.js
@@ -1,30 +1,39 @@
 const express = require("express");
 const app = express();
+
 const bodyParser = require("body-parser");
 const path = require("path");
 const fs = require("fs");
+
 const cors = require("cors");
 app.use(
   cors({
     origin: "*",
   })
 );
+
 const dotenv = require("dotenv");
 dotenv.config();
+
 const sequelize = require("./util/database");
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 //Router
 const userRouter = require("./router/userRouter");
+const homePageRouter = require("./router/homePageRouter");
 //Models
 
 //Middleware
 app.use("/", userRouter);
 app.use("/user", userRouter);
 
+app.use("/homePage", homePageRouter);
+
 sequelize
-  .sync()  
+  .sync()
   .then((result) => {
     app.listen(process.env.PORT || 4000);
   })
